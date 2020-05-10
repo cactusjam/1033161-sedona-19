42623614fe6a071b410a694b5be85dd9a9b1c5ea
Evgenia Alfimova <alfimova.evg@gmail.com>
2020-05-10 18:35:29 +0300

    форма планшетная версия

--- source/js/script.js
+++ source/js/script.js
@@ -30,75 +30,10 @@ btnOpen.addEventListener('click', function () {
 });
 
 // ------отправка формы-----
-// var link = document.querySelector(".review-form__btn");
-// var popup = document.querySelector(".popup");
-// var form = popup.querySelector("review-form");
-// var login = popup.querySelector("[name=user_name]");
-// var email = popup.querySelector("[name=user_email]");
-// var surname = popup.querySelector("[name=user_surname]")
 
-// link.addEventListener("click", function (evt) {
-// 	evt.preventDefault();
-// 	form.addEventListener("submit", function (evt) {
-//     if (!login.value || !email.value || !message.value) {
-//       evt.preventDefault();
-//       popup.classList.remove("modal_error");
-//       popup.offsetWidth = popup.offsetWidth;
-//       popup.classList.add("modal_error");
-//       if (login.value === '') {
-//         login.classList.add('popup-invalid');
-//       } else {
-//         login.classList.remove('popup__invalid');
-//       }
-//       if (email.value === '') {
-//         email.classList.add('popup__invalid');
-//       } else {
-//         email.classList.remove('popup__invalid');
-//       }
-//       if (message.value === '') {
-//         surname.classList.add('popup__invalid');
-//       } else {
-//         surname.classList.remove('popup__invalid');
-//       }
-//     }
-//   });
-// });
-
-// form.addEventListener("submit", function (evt) {
-// 	if (!login.value || !email.value || !message.value) {
-// 		evt.preventDefault();
-// 		popup.classList.remove("modal_error");
-// 		popup.offsetWidth = popup.offsetWidth;
-// 		popup.classList.add("modal_error");
-// 		if (login.value === '') {
-// 			login.classList.add('popup-invalid');
-// 		} else {
-// 			login.classList.remove('popup__invalid');
-// 		}
-// 		if (email.value === '') {
-// 			email.classList.add('popup__invalid');
-// 		} else {
-// 			email.classList.remove('popup__invalid');
-// 		}
-// 		if (message.value === '') {
-// 			surname.classList.add('popup__invalid');
-// 		} else {
-// 			surname.classList.remove('popup__invalid');
-// 		}
-// 	}
-// });
-
-// window.addEventListener("keydown", function (evt) {
-// 	if (evt.keyCode === 27) {
-// 		evt.preventDefault();
-// 		if (popup.classList.contains("modal_show")) {
-// 			popup.classList.remove("modal_show");
-// 			popup.classList.remove("modal_error");
-// 		}
-// 	}
-// });
 var popup = document.querySelector(".popup");
 var form = document.querySelector(".review-form");
+
 if (form) {
   var link = document.querySelector(".review-form__btn");
   var errorPopup = document.querySelector(".popup--mistake");
@@ -115,14 +50,14 @@ if (form) {
     }
   });
 
-  var errorBtn = errorPopup.querySelector(".popup__btn-ok");
+  var errorBtn = errorPopup.querySelector(".popup__btn--ok");
 
   errorBtn.addEventListener("click", function(event) {
         event.preventDefault();
         errorPopup.classList.remove("popup__show");
     })
 
-    var successBtn = successPopup.querySelector(".popup__btn-close");
+    var successBtn = successPopup.querySelector(".popup__btn--close");
 
     successBtn.addEventListener("click", function(event) {
         event.preventDefault();
